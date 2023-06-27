afc006033890a5ef2f9f2b5fc1c162cc21a1c34e
franklin-sun <ssss_100@live.cn>
2023-06-28 02:00:58 +0800

    Version 0.1

--- Zeiss/views.py
+++ Zeiss/views.py
@@ -7,6 +7,7 @@ import json
 from django.shortcuts import get_object_or_404
 import pandas as pd
 import datetime
+from Zeiss import functions, nlptest
 
 def hello(request):
     return HttpResponse("Hello world ! ")
@@ -17,11 +18,19 @@ def input(request):
 
 def comment(request):
     context = request.POST['context']
+    name = request.POST['name']
+    if name == '':
+        name = '匿名用户'
+    date_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     # 写入数据库
-    print(context)
+    print(name, context, date_time)
+    functions.write_db(name, context, date_time)
     return render(request, 'comment.html')
 
 def result(request):
     # Get latest result
-    return render(request, 'result.html')
+    fc = functions.read_db()
+    comment1,comment2,comment3 = fc.get_comments()
+    nlptest
+    return render(request, 'result.html', {"comment1": comment1, "comment2": comment2, "comment3": comment3})
 
